07c30c3b38b87644a68917ec3ddde287c00af020
Adrian Ottiker <adrian@ottiker.com>
2014-02-21 18:22:42 +0100

    load view

--- login.js
+++ login.js
@@ -5,6 +5,15 @@ var View = require('github/jillix/view/v0.0.1/view');
 function init () {
     var self = this;
     var config = self.mono.config.data;
+    
+    if (!config.view) {
+        return console.error('[login: no view config]');
+    }
+    
+    // init view
+    View(self).load(config.view, function (err) {
+        // get dom refs
+    });
 }
 
 module.exports = init;
