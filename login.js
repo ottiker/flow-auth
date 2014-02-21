cb48da13b78ad0591d5d2d34387642e33e015a29
Adrian Ottiker <adrian@ottiker.com>
2014-02-21 17:00:10 +0100

    created client module file

---
+++ login.js
@@ -0,0 +1,14 @@
+M.wrap('github/jillix/login/v0.0.1/login.js', function (require, module, exports) {
+
+var View = require('github/jillix/view/v0.0.1/view');
+
+function init () {
+    var self = this;
+    var config = self.mono.config.data;
+}
+
+module.exports = init;
+
+return module;
+
+});
