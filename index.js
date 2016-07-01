3d0125a268cfc3d2e585e3387e6ab9bbda726984
Adrian Ottiker <adrian@ottiker.com>
2016-07-01 13:55:30 +0000

    copied client-session code from flow-http

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-// cookie handling
-var SID = {
+var sessions = require('client-sessions');
+var crypto = require('crypto');
 
-    get: function (name) {
-        // credentials: https://developer.mozilla.org/en-US/docs/Web/API/Document.cookie
-        return document.cookie.replace(new RegExp('(?:(?:^|.*;)\\s*' + name.replace(/[\-\.\+\*]/g, '\\$&') + '\\s*\\=\\s*([^;]*).*$)|^.*$'), '$1') || null;
-    },
-
-    set: function (name, value) {
-        document.cookie = name + '=' + value + ';path=/';
-    },
-
-    rm: function (name) {
-        document.cookie = name + '=;path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+// default session config
+var defaultSession = {
+    cookieName: 'SES', // cookie name dictates the key name added to the request object
+    requestKey: 'session', // requestKey overrides cookieName for the key name added to the request object
+    secret: crypto.randomBytes(64).toString('hex'), // should be a large unguessable string
+    duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
+    activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+    cookie: {
+        ephemeral: true, // when true, cookie expires when the browser closes
+        httpOnly: true, // when true, cookie is not accessible from javascript
+        secure: true // when true, cookie will only be sent over SSL. use key 'secureProxy' instead if you handle SSL not in your node process
     }
 };
 
-// set or remove cookie session
-exports.session = function (_options, data, next) {
+var clientSession = sessions(defaultSession);
 
-    options = _options || {
-        sid: this._config.cookie || 'sid'
-    };
-    
-    // set session
-    if (!options.remove && data && data[options.sid]) {
-        SID.set(options.sid, data[options.sid]);
+exports.session = {
 
-    // remove session
-    } else {
-        SID.rm(options.sid);
-    }
-    
-    next(null, data)
+    // get session data
+	get: function (options, data, next) {
+        clientSession(data.req, data.res, function () {
+			data.session = req.session
+		    next(null, data);
+        });
+	},
+
+    // set session data
+	set: function () {},
+
+    // destroy the session
+	destroy: function () {}
+};
+
+exports.token = {
+	create: function () {},
+	validate: function () {}
 };
